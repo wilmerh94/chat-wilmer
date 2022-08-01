0e9f86ed9c644a9ed13e40f86865067e2aea0c33
Wilmer <herrerawilmer.94@gmail.com>
2022-08-01 12:05:26 -0600

    adding comments

--- src/Hooks/useLogout.jsx
+++ src/Hooks/useLogout.jsx
@@ -1,53 +1,51 @@
-import { getAuth } from 'firebase/auth'
-import { doc, updateDoc } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
-import { toast } from 'react-toastify'
-import { db } from '../firebase/config'
+import { getAuth } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { db } from '../firebase/config';
 
-import { useAuthContext } from './useAuthContext'
+import { useAuthContext } from './useAuthContext';
 
 export const useLogout = () => {
-  const auth = getAuth()
-  const [isCancelled, setIsCancelled] = useState(false)
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [isCancelled, setIsCancelled] = useState(false);
 
-  const [error, setError] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
-  const { dispatch, user } = useAuthContext()
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { dispatch, user } = useAuthContext();
 
   const logOut = async () => {
-    setError(null)
-    setIsLoading(true)
+    setError(null);
+    setIsLoading(true);
 
     //   Sign the user out
     try {
-      const { uid } = user
-      console.log(uid)
+      const { uid } = user;
       // update online status
-      const docRef = await doc(db, 'users', uid)
+      const docRef = await doc(db, 'users', uid);
       await updateDoc(docRef, {
         online: false
-      })
+      });
 
-      await auth.signOut()
+      await auth.signOut();
       //  Dispatch Log Out
-      dispatch({ type: 'LOGOUT' })
-      if (isCancelled) {
-        //  Update state
-        setIsLoading(false)
-        setError(null)
-      }
+      dispatch({ type: 'LOGOUT' });
+      //  Update state
+      setIsLoading(false);
+      setError(null);
+      navigate('/login');
     } catch (error) {
-      if (!isCancelled) {
-        console.log(error.message)
-        toast.error(error)
-        setError(error)
-        setIsLoading(false)
-      }
+      console.log(error.message);
+      toast.error(error);
+      setError(error);
+      setIsLoading(false);
     }
-  }
+  };
   useEffect(() => {
-    return () => setIsCancelled(true)
-  }, [])
+    return () => setIsCancelled(true);
+  }, []);
 
-  return { logOut, error, isLoading }
-}
+  return { logOut, error, isLoading };
+};
